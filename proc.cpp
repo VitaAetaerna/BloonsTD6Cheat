8aad400bcb73ca3f3f8f330ec4cc3a3649293767
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-17 22:03:34 +0100

    Update proc.cpp

--- proc.cpp
+++ proc.cpp
@@ -1,1 +1,28 @@
+#include "proc.h"
 
+DWORD getProcessID(const wchar_t* procName) {
+
+	DWORD procID = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(PROCESSENTRY32);
+
+		if (Process32First(hSnap, &procEntry)) {
+			do {
+				if (!_wcsicmp(procEntry.szExeFile, procName)) {
+					procID = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+
+
+	}
+	else {
+		return 0;
+	}
+
+
+}
